84a6688f986569f01c0564b58d468e4d77108ce2
Savannah Scott <sascott624@gmail.com>
2016-07-17 15:22:16 +0000

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,20 @@
 'use strict';
+
+function favIceCream(flavor){
+  if (typeof flavor === "undefined"){
+    flavor = "Chocolate";
+  }
+  return "I love " + flavor;
+}
+
+function shouting(string){
+  return string.toUpperCase();
+}
+
+function roundDown(number){
+  return Math.floor(number);
+}
+
+function theTruth(){
+  return true;
+}
